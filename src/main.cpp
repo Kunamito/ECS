012898968628a18de04cece186cfc8fdbea73354
JulesDELTEIL <jules.delteil@epitech.eu>
2025-06-26 13:25:42 +0200

    feat: base project

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2025
+** ECS
+** File description:
+** main.cpp
+*/
+
+#include <iostream>
+
+#include "projectTools.hpp"
+
+int main(int, const char **)
+{
+    std::cout << "Projet test, c'est public" << std::endl;
+    return EXIT_SUCCESS;
+}
