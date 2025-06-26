1f319d87c68a996685650b87cc91538ea1c0ef57
JulesDELTEIL <jules.delteil@epitech.eu>
2025-06-26 13:44:39 +0200

    feat: tkt mon gah

--- include/SparseArray.hpp
+++ include/SparseArray.hpp
@@ -22,28 +22,70 @@ class SparseArray {
         using const_iterator = typename container_t::const_iterator;
     
     public:
-        SparseArray(); // You can add more constructors .
-        SparseArray(SparseArray const &); // copy constructor
-        SparseArray(SparseArray &&) noexcept; // move constructor
-        ~SparseArray();
-        SparseArray& operator=(SparseArray const &); // copy assignment operator
-        SparseArray& operator=(SparseArray &&) noexcept; // move assignment operator
-        reference_type operator[](size_t idx);
-        const_reference_type operator[](size_t idx) const;
-        iterator begin();
-        const_iterator begin() const;
-        const_iterator cbegin() const;
-        iterator end();
-        const_iterator end() const;
-        const_iterator cend() const;
-        size_type size() const;
-        reference_type insert_at(size_type pos, Component const &);
-        reference_type insert_at(size_type pos, Component &&);
+        SparseArray() {
+
+        } // You can add more constructors .
+        SparseArray(SparseArray const &) {
+
+        } // copy constructor
+        SparseArray(SparseArray &&) noexcept {
+
+        } // move constructor
+        ~SparseArray() {
+
+        }
+
+        SparseArray& operator=(SparseArray const &) {
+
+        } // copy assignment operator
+        SparseArray& operator=(SparseArray &&) noexcept {
+
+        } // move assignment operator
+        reference_type operator[](size_t idx) {
+
+        }
+        const_reference_type operator[](size_t idx) const {
+
+        }
+
+        iterator begin() {
+
+        }
+        const_iterator begin() const {
+
+        }
+        const_iterator cbegin() const {
+
+        }
+        iterator end() {
+
+        }
+        const_iterator end() const {
+
+        }
+        const_iterator cend() const {
+
+        }
+        size_type size() const {
+
+        }
+        reference_type insert_at(size_type pos, Component const &) {
+
+        }
+        reference_type insert_at(size_type pos, Component &&) {
+
+        }
 
         template<class ...Params>
-        reference_type emplace_at(size_type pos, Params &&...); // optional
-        void erase(size_type pos);
-        size_type get_index(value_type const &) const;
+        reference_type emplace_at(size_type pos, Params &&...) {
+
+        } // optional
+        void erase(size_type pos) {
+
+        }
+        size_type get_index(value_type const &) const {
+
+        }
     
     private:
         container_t _data;
