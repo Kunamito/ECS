3511a4543e754f4c6f03d9cd4b8e3d067b3ecef4
JulesDELTEIL <jules.delteil@epitech.eu>
2025-06-26 13:26:25 +0200

    fix: remove debug test

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,9 @@
 ** main.cpp
 */
 
-#include <iostream>
-
 #include "projectTools.hpp"
 
 int main(int, const char **)
 {
-    std::cout << "Projet test, c'est public" << std::endl;
     return EXIT_SUCCESS;
 }
